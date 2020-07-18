5a004f0909260576253922676cc32c33920ede33
fabianzentorres <67809928+fabianzentorres@users.noreply.github.com>
2020-07-17 22:00:42 -0400

    Agrege area Recepcionista

---
+++ Clinica/Areas/Recepcionista/Controllers/TratamientosController.cs
@@ -0,0 +1,90 @@
+﻿using Clinica.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Clinica.Areas.Recepcionista.Controllers
+{
+    public class TratamientosController : Controller
+    {
+        private ClinicaContext _db = null;
+        // GET: Pacientes
+        public ActionResult Index()
+        {
+            IEnumerable<Tratamiento> tratamientos = null;
+            using (_db = new ClinicaContext())
+            {
+                tratamientos = _db.Tratamientos.ToList();
+            }
+            return View(tratamientos);
+        }
+
+        //#######################################
+
+        [HttpGet]
+        public ActionResult Create()
+
+        {
+            Tratamiento tratamiento = new Tratamiento();
+
+            return View(tratamiento);
+        }
+
+        [HttpPost]
+        public ActionResult Create(Tratamiento tratamiento)
+        {
+            if (ModelState.IsValid)
+            {
+                //guarda en bd
+                return RedirectToAction("Index", "Tratamientos", new { id = 1 });
+            }
+
+            return View(tratamiento);
+        }
+
+
+        //########################################################
+
+
+
+
+        public ActionResult View(int id)
+        {
+            Tratamiento t = null;
+            using (_db = new ClinicaContext())
+            {
+                t = _db.Tratamientos.Find(id);
+            }
+            return View(t);
+        }
+
+
+        //###################################################
+
+        [HttpGet]
+        public ActionResult Edit(int id)
+        {
+
+            Tratamiento t = null;
+            using (_db = new ClinicaContext())
+            {
+                t = _db.Tratamientos.Find(id);
+            }
+            return View(t);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Tratamiento tratamiento)
+        {
+            if (ModelState.IsValid)
+            {
+                //guarda en bd
+                return RedirectToAction("View", "Tratamientos", new { id = 1 });
+            }
+
+            return View(tratamiento);
+        }
+    }
+}
---
+++ Clinica/Areas/Recepcionista/RecepcionistaAreaRegistration.cs
@@ -0,0 +1,25 @@
+﻿using System.Web.Mvc;
+
+namespace Clinica.Areas.Recepcionista
+{
+    public class RecepcionistaAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "Recepcionista";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "Recepcionista_default",
+                "Recepcionista/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional },
+                namespaces: new[] { "Clinica.Areas.Recepcionista.Controllers" }
+            );
+        }
+    }
+}
