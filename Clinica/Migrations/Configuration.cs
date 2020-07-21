dff230ad161bf3ee31461125860985a1395cd8ab
andres2485 <67347368+andres2485@users.noreply.github.com>
2020-07-20 20:31:30 -0400

    Archivo configuracion Migration

--- Clinica/Migrations/Configuration.cs
+++ Clinica/Migrations/Configuration.cs
@@ -1,5 +1,6 @@
 ﻿namespace Clinica.Migrations
 {
+    using Clinica.Models;
     using System;
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
@@ -9,15 +10,84 @@
     {
         public Configuration()
         {
-            AutomaticMigrationsEnabled = false;
+            AutomaticMigrationsEnabled = true;
         }
 
         protected override void Seed(Clinica.Models.ClinicaContext context)
         {
-            //  This method will be called after migrating to the latest version.
 
-            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
-            //  to avoid creating duplicate seed data.
+            // Odontologos
+            Odontologo o1  = new Odontologo() 
+            
+            { Nombres = "Andres" ,
+            Apellidos="Garcia",
+            Telefonos="569123456789",
+            Direcciones="Chiguayanate",
+            Correos="c@ejemplo.cl",
+            Rut="16037888-5",
+            FechaIngreso="17-07-2020",
+            Edad="30",
+            Especialidad="Endodoncia"
+            
+            };
+
+            Odontologo o2 =new Odontologo()
+
+            {
+                Nombres = "Guillermo",
+                Apellidos = "Ramirez",
+                Telefonos = "569123456789",
+                Direcciones = "Santiago",
+                Correos = "Guillermo@ejemplo.cl",
+                Rut = "123456789",
+                FechaIngreso = "19-07-2020",
+                Edad = "40",
+                Especialidad = "Limpieza General"
+
+            };
+
+            context.Odontologos.Add(o1);
+            context.Odontologos.Add(o2);
+            context.SaveChanges();
+
+
+
+            // Tratamientos 
+
+
+
+            Tratamiento t1 = new Tratamiento()
+            {
+
+                Nombres = "Ndodoncia",
+                Valor = "80.000",
+                Especialistas = "JPErez",
+                Codigos = "00001",
+                Descripcion = "Tratamiento",
+            Horarios=" L a M 10:00 - 15:00",
+            FechaCreacion="17-07-2020"
+
+
+            };
+
+            Tratamiento t2 = new Tratamiento()
+            {
+
+                Nombres = "Ortodoncia",
+                Valor = "150.000",
+                Especialistas = "Cristian Perez",
+                Codigos = "00003",
+                Descripcion = "Trtamiento",
+                Horarios = " Lunes 10:00 A.m ",
+                FechaCreacion = "19-07-2020"
+
+
+            };
+
+            context.Tratamientos.Add(t1);
+            context.Tratamientos.Add(t2);
+            context.SaveChanges();
+
         }
     }
 }
